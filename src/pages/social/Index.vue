ecb1bf2c5447804b46bb84378a6cdb0b972f8b3a
Yao Ran <7833754+shukuang_yao@user.noreply.gitee.com>
2020-11-30 11:09:09 +0800

    试下

--- src/pages/social/Index.vue
+++ src/pages/social/Index.vue
@@ -35,25 +35,25 @@
                 </el-tab-pane>
                 <el-tab-pane :label="'关注 '+user.following" name="following" style="padding: 5px">
                     <div>
-                        <div v-if="$page.followings.edges.length">
+                        <div v-if="followings.length">
                             <el-row style="min-height: 200px; ">
-                                <el-col :span="8" v-for="(item,index) in $page.followings.edges" :key="'following'+index" style="padding: 10px">
+                                <el-col :span="8" v-for="(item,index) in followings" :key="'following'+index" style="padding: 10px">
                                     <el-card shadow="hover" style="font-size: 13px;color: #606266;line-height: 20px">
                                         <i class="el-icon-star-off"></i>&emsp;
-                                        <g-link :to="'/social/details/'+item.node.login"  style=" text-decoration:none;cursor:pointer">{{item.node.login}}</g-link>
+                                        <router-link :to="'/social/details/'+item.login"  style=" text-decoration:none;cursor:pointer">{{item.login}}</router-link>
                                         <br>
                                         <i class="el-icon-message"></i>&emsp;
-                                        <a :href="item.node.html_url" target="_blank" style=" text-decoration:none;cursor:pointer">TA的主页</a>
+                                        <a :href="item.html_url" target="_blank" style=" text-decoration:none;cursor:pointer">TA的主页</a>
                                         <br>
-                                        <img :src="item.node.avatar_url" style="width: 100%;border-radius:5px;margin-top: 5px">
+                                        <img :src="item.avatar_url" style="width: 100%;border-radius:5px;margin-top: 5px">
                                     </el-card>
                                 </el-col>
                             </el-row>
                             <div style="text-align: center;margin-top: 10px">
                                 <el-pagination 
                                     @current-change="onSelect" background layout="prev, pager, next" 
-                                    :current-page="$page.followings.pageInfo.currentPage"
-                                    :page-size="9" :total="$page.followings.pageInfo.totalPages * 9">
+                                    :current-page="$route.params.page || 1"
+                                    :page-size="9" :total="user.following">
                                 </el-pagination>
                             </div>
                         </div>
@@ -75,6 +75,7 @@ query ($page: Int) {
             node{
                 followers
                 following
+                githubUsername
             }
         }
     }
@@ -92,23 +93,10 @@ query ($page: Int) {
             }
         }
     }
-    followings: allFollowing(perPage: 9, page: $page) @paginate{
-        pageInfo {
-            totalPages
-            currentPage
-        }
-        edges{
-            node{
-                id
-                login
-                html_url
-                avatar_url
-            }
-        }
-    }
 }
 </page-query>
 <script>
+import {request} from '@/utils/request'
     export default {
         name: 'socialPage',
         metaInfo:{
@@ -117,12 +105,17 @@ query ($page: Int) {
         data () {
             return {
                 activeTab: this.$route.query.activeTab || "followers",
+                followings:[]
             }
         },
         computed: {
             user(){
                 return this.$page.users.edges[0].node
             }
+        },
+        async mounted () {
+            const {data} = await request.get(`users/${this.user.githubUsername}/following?page=${this.$route.params.page || 1}&per_page=9`)
+            this.followings = data
         },
         watch: {
             activeTab:function(newVal){
